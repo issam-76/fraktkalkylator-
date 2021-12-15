2aba4347ec85b0f9c5a9e014f4ede72d5d96c0c9
sam se <seissam@gmail.com>
2021-12-15 07:00:36 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
 
      const [resultatValue, setresultatValue] = useState("");
 
-    var [badge, Setbadge] = useState("");
+   
 
     const [firstSelectValue, setFirstSelectValue] = useState("");
     const [codeFrom, setcodeFrom] = useState("");
