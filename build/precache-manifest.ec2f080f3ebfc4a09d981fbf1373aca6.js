931fc494efa9f4aa12c5547882db0f4bdacad4ee
sam se <seissam@gmail.com>
2021-12-15 06:40:29 +0100

    Add files via upload

---
+++ build/precache-manifest.ec2f080f3ebfc4a09d981fbf1373aca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8023c6a01ad7875ce18de1c6c92dfeff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5457a07596484bfdf70b",
+    "url": "/static/css/main.b7d0b181.chunk.css"
+  },
+  {
+    "revision": "76331192e5ffc30723ea",
+    "url": "/static/js/2.6c23751d.chunk.js"
+  },
+  {
+    "revision": "5457a07596484bfdf70b",
+    "url": "/static/js/main.20b50469.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
